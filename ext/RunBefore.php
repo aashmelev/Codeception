3fbdf43c3574e208c7ae079a27f116fc6066493a
Alexey Shmelev <a.a.shmelev@gmail.com>
2018-07-18 07:33:35 +0300

    Возможность добавления синхронных и асинхронных процессов
    Будет нужен рефакторинг

--- ext/RunBefore.php
+++ ext/RunBefore.php
@@ -18,7 +18,11 @@ use Symfony\Component\Process\Process;
  * extensions:
  *     enabled:
  *         - Codeception\Extension\RunBefore:
- *             - yarn run build-dev
+ *             - async_proc1
+ *             -
+ *                 - sync_proc1
+ *                 - sync_proc2
+ *             - async_proc2
  * ```
  *
  * HINT: you can use different configurations per environment.
@@ -31,6 +35,9 @@ class RunBefore extends Extension
         Events::SUITE_BEFORE => 'runProcess'
     ];
 
+    /** @var Process[] */
+    protected $processes = [];
+
     public function _initialize()
     {
         if (!class_exists('Symfony\Component\Process\Process')) {
@@ -40,11 +47,61 @@ class RunBefore extends Extension
 
     public function runProcess()
     {
+        $this->processes = [];
         foreach ($this->config as $key => $command) {
-            $process = new Process($command, $this->getRootDir(), null, null, null);
-            $this->output->debug('[RunBefore] Starting ' . $command);
-            $process->run();
-            $this->output->debug('[RunBefore] Completing ' . $process->getCommandLine());
+            if (is_array($command)) {
+                $currentCommand = array_shift($command);
+                $process = new Process($currentCommand, $this->getRootDir(), null, null, null);
+                $this->output->debug('[RunBefore] Starting ' . $currentCommand);
+                $process->start();
+                $nextProcesses = $command;
+            } else {
+                $process = new Process($command, $this->getRootDir(), null, null, null);
+                $this->output->debug('[RunBefore] Starting ' . $command);
+                $process->start();
+                $nextProcesses = [];
+            }
+
+            $this->processes[] = [
+                'process' => $process,
+                'next' => $nextProcesses
+            ];
         }
+
+        while (count($this->processes) !== 0) {
+            $this->checkProcesses();
+            sleep(1);
+        }
+    }
+
+    private function checkProcesses()
+    {
+        foreach ($this->processes as $key => $process) {
+            if (!$this->isRunning($process['process'])) {
+                $this->output->debug('[RunBefore] Completing ' . $process['process']->getCommandLine());
+
+                if ($currentCommand = array_shift($process['next'])) {
+                    $currentProcess = new Process($currentCommand, $this->getRootDir(), null, null, null);
+                    $this->output->debug('[RunBefore] Starting ' . $currentCommand);
+                    $currentProcess->start();
+                    $nextProcesses = $process['next'];
+
+                    $this->processes[] = [
+                        'process' => $currentProcess,
+                        'next' => $nextProcesses
+                    ];
+                }
+
+                unset($this->processes[$key]);
+            }
+        }
+    }
+
+    private function isRunning(Process $process) {
+        if ($process->isRunning()) {
+            return true;
+        }
+
+        return false;
     }
 }
